944f7df2f5212dd8ae3b42fdd0eb8202e50bb963
Roman Markov <rmarkov@c4b3c1.42berlin.de>
2025-12-19 15:06:29 +0100

    added headers

--- include/pipex.h
+++ include/pipex.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarkov <rmarkov@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/19 15:05:25 by rmarkov           #+#    #+#             */
+/*   Updated: 2025/12/19 15:05:27 by rmarkov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PIPEX_H
 # define PIPEX_H
 
@@ -7,7 +19,6 @@
 # include <stdlib.h>
 # include <sys/wait.h>
 # include <fcntl.h>
-
 
 void	execute_cmd(char *cmd, char **envp);
 
